b12365b59f17094d9c28ff58038fefaa4b2ae9e2
experimentoltpb <98768754+experimentoltpb@users.noreply.github.com>
2022-02-15 17:19:49 -0300

    dados JG
    Dados extraidos do Jogo e códigos de analise atualizados

---
+++ Rotina_Dados _Quest/Formatacao_JG.py
@@ -0,0 +1,55 @@
+import os
+import csv
+from operator import index
+import numpy as np
+import pandas as pd
+
+
+#cria uma nova pasta dentro da pasta do projeto
+#os.makedirs('C:/Users/Numec/Documents/GitHub/main/Rotina_Dados _Quest/dados/processados', exist_ok=True)
+
+
+# create empty lists
+dataframes_list = []
+Dset = pd.DataFrame()
+Dset_list = []
+
+#gerando a lista com todos os arquivos
+path=("C:/Users/Numec/Documents/GitHub/main/Rotina_Dados _Quest/dados")
+path2=("C:/Users/Numec/Documents/GitHub/main/Rotina_Dados _Quest/processed")
+files = os.listdir(path)
+file_count = len(files)
+print(file_count)
+for j in range(file_count):
+    temp_df = pd.read_csv(path +"/"+ files[j])
+    dataframes_list.append(temp_df)
+
+pd.set_option("display.max_rows", None, "display.max_columns", None) #pra os dados serem escritos além do limite de exibição do python#
+Dsetwlist=open("dataframe_full.txt",'w')    #cria o arquivo com todos os dados
+#reorganizando e escrevendo as tabelas para ficarem compatíveis às rotinas de análise
+for dataset in dataframes_list:
+        for i in range(dataset.shape[0]):  # Número de linhas
+            aux = list()
+            aux = aux + [1] + [1]
+            aux = aux + [str(dataset.iloc[i, 0])]  # coluna de movimentos
+            aux = aux + [1]  # Coluna step
+            aux = aux + [12]  # 12 é o identificador da árvore
+            aux = aux + [1]  # adicionando identificação alternativa do participante
+            aux = aux + [str(dataset.iloc[i, 3])]  # adicionando coluna de tempos de respostas
+            aux = aux + [str(dataset.iloc[i, 2])]  # adicionando coluna de escolhas dos participantes
+            aux = aux + [str(dataset.iloc[i, 1])]  # adicionando a cadeia do batedor
+            data2set = pd.DataFrame(aux)
+            frames = [Dset, data2set.T]
+            Dset = pd.concat(frames)
+            Dset = Dset.reset_index(drop='True')
+Dsetwlist.write(str(Dset))
+Dset_list.append(Dset)
+Dsetwlist.close()
+#criando os arquivos individuais
+df_full = pd.read_csv("dataframe_full.txt" )
+for i in range (file_count):
+    df_full = df_full.reset_index(drop='True')
+    Df_set=df_full[:1000].to_csv(path2 + "/" +files[i],sep=',',index=False)
+    df_full = df_full.drop(df_full.index[range(1000)])
+    print(Df_set)
+
