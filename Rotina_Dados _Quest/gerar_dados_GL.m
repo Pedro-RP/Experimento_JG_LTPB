a61210c1fc83163fb0f7480a946b96432945dc10
Pedro-RP <ribeirop406@gmail.com>
2022-02-19 08:20:45 -0300

    Update gerar_dados_GL.m

--- Rotina_Dados _Quest/gerar_dados_GL.m
+++ Rotina_Dados _Quest/gerar_dados_GL.m
@@ -1,26 +1,28 @@
 %%%Preparando dados para inserção no Goalkeeper's Lab
 
-%Grupo Controle
+%%Grupo Controle
 
 predata_control = readtable('C:\Users\Pedro_R\Desktop\Projeto\Code_exp_ltpb\Rotina_Dados _Quest\dataframe_Control.csv');
 predata_control(: , 1) = [ ]; 
+predata_control(: , 1) = [ ];
+predata_control(: , 10) = [ ];
 data_control = table2array(predata_control);
-data_control(: , 1) = [ ];
 
-%Grupo LTPB
+
+%%Grupo LTPB
 
 predata_ltpb = readtable('C:\Users\Pedro_R\Desktop\Projeto\Code_exp_ltpb\Rotina_Dados _Quest\dataframe_Patient.csv');
 predata_ltpb(: , 1) = [ ];
+predata_ltpb(: , 1) = [ ];
+predata_ltpb(: , 10) = [ ];
 data_ltpb = table2array(predata_ltpb);
-data_ltpb(: , 1) = [ ];
+
 
 %%Lançar software para grupo Controle:
 
-%data = data_control; %o nome da variável tem que ser data para rodar no software
-%Presoftware(data)
+%Presoftware(data_control)
 
 %%Lançar Software para grupo LTPB:
 
-%data=data_ltpb;
-%Presoftware(data)
+%Presoftware(data_ltpb)
 
