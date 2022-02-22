2303ce55516790304007dc31f122c551ec30631b
Pedro-RP <ribeirop406@gmail.com>
2022-02-22 19:42:04 -0300

    padronizando os eixos dos gráficos da média temporal móvel

--- Rotina_Dados _Quest/analises_e_graficos.m
+++ Rotina_Dados _Quest/analises_e_graficos.m
@@ -1,37 +1,38 @@
-%%%Preparando dados para inser√ß√£o no Goalkeeper's Lab
+%%%Preparando dados para inserÁ„o no Goalkeeper's Lab
 
 %%Dados COmpletos
-predata_full = readtable('C:\Users\Numec\Documents\GitHub\main\Rotina_Dados _Quest\dataframe_full.csv'); 
+predata_full = readtable('C:\Users\Pedro_R\Desktop\Projeto\Code_exp_ltpb\Rotina_Dados _Quest\dataframe_full.csv'); 
 predata_full(: , 1) = [ ];
 data_full = table2array(predata_full);
 
 %%Grupo Controle
 
-predata_control = readtable('C:\Users\Numec\Documents\GitHub\main\Rotina_Dados _Quest\dataframe_Control.csv');
+predata_control = readtable('C:\Users\Pedro_R\Desktop\Projeto\Code_exp_ltpb\Rotina_Dados _Quest\dataframe_Control.csv');
 predata_control(: , 1) = [ ]; 
 predata_control(: , 1) = [ ];
 data_control = table2array(predata_control);
 
 %%Grupo LTPB
-predata_ltpb = readtable('C:\Users\Numec\Documents\GitHub\main\Rotina_Dados _Quest\dataframe_Patient.csv');
+predata_ltpb = readtable('C:\Users\Pedro_R\Desktop\Projeto\Code_exp_ltpb\Rotina_Dados _Quest\dataframe_Patient.csv');
 predata_ltpb(: , 1) = [ ];
 predata_ltpb(: , 1) = [ ];
 data_ltpb = table2array(predata_ltpb);
 
 
-%%Lan√ßar software para grupo Controle:
+%%LanÁar software para grupo Controle:
 
 %Presoftware(data_control)
 
-%%Lan√ßar Software para grupo LTPB:
+%%LanÁar Software para grupo LTPB:
 
-Presoftware(data_ltpb)
+%Presoftware(data_ltpb)
 
-%%%%Construindo a m√©dia m√≥vel
+%%%%Construindo a mÈdia mÛvel
+
 Z=data_control;  %arquivo sendo utilizado
 
-% 
-% %Construindo W ,como vetor de erros e acertos
+
+%Construindo W ,como vetor de erros e acertos
 % for i = 1:size(Z,1);
 %   
 %    if Z(i,8)== Z(i,9);
@@ -45,7 +46,7 @@ Z=data_control;  %arquivo sendo utilizado
 % %display(W)
 % %T=movmean(W,101);
 % 
-% %Definindo a Cell M composta pelos vetores m equivalentes a m√©dia m√≥vel de
+% %Definindo a Cell M composta pelos vetores m equivalentes a mÈdia mÛvel de
 % %cada participante
 % %m = zeros(1,size(Z,2));
 % i=1;
@@ -62,7 +63,7 @@ Z=data_control;  %arquivo sendo utilizado
 % %Construindo os graficos
 % x = linspace(1,1000,1000);
 % 
-% title('m√©dia m√≥vel(tx de acerto)')
+% title('MÈdia MÛvel de Acur·cia')
 % 
 % hold on
 % for i= 1:(size(Z,1)/1000) ;
@@ -73,13 +74,13 @@ Z=data_control;  %arquivo sendo utilizado
 % hold off
 
 
-%%% Contruindo m√©dio m√≥vel temporal
+%%% Contruindo mÈdio mÛvel temporal
 for i = 1:size(Z,1);
   P(i)=Z(i,7);
 %
    end
    
-%Definindo a Cell M composta pelos vetores m equivalentes a m√©dia m√≥vel de
+%Definindo a Cell M composta pelos vetores m equivalentes a mÈdia mÛvel de
 %cada participante
 
 i=1;
@@ -95,11 +96,12 @@ end
 %Construindo os graficos
 x = linspace(1,1000,1000);
 
-title('m√©dia m√≥vel temporal')
+title('MÈdia MÛvel Temporal')
 
 hold on
 for i= 1:(size(Z,1)/1000) ;
 plot(x,N{i})
+ylim ([0 4])
 
 end
 
