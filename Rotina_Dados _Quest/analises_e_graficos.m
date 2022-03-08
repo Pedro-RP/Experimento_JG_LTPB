717ea2cf0515e9e9f45e797149b32a4c51106133
Pedro-RP <ribeirop406@gmail.com>
2022-03-08 17:55:04 -0300

    adição de marcadores de intervalos e melhora nas cores utilizadas

--- Rotina_Dados _Quest/analises_e_graficos.m
+++ Rotina_Dados _Quest/analises_e_graficos.m
@@ -7,7 +7,7 @@ data_full = table2array(predata_full);
 
 %%Grupo Controle
 
-predata_control = readtable('C:\Users\Pedro_R\Desktop\Projeto\Code_exp_ltpb\Rotina_Dados _Quest\dataframe_Patient.csv');
+predata_control = readtable('C:\Users\Pedro_R\Desktop\Projeto\Code_exp_ltpb\Rotina_Dados _Quest\dataframe_control.csv');
 predata_control(: , 1) = [ ]; 
 predata_control(: , 1) = [ ];
 data_control = table2array(predata_control);
@@ -31,28 +31,79 @@ data_ltpb = table2array(predata_ltpb);
 
 Z=data_full;  %arquivo sendo utilizado
 
-% %Construindo W ,como vetor de erros e acertos
+%Construindo W ,como vetor de erros e acertos
+for i = 1:size(Z,1);
+  
+   if Z(i,8)== Z(i,9);
+       W(i)= 1;    
+   else 
+       W(i)= 0;
+   end
+   
+   
+end
+
+
+%Definindo a Cell M composta pelos vetores m equivalentes a média móvel de
+%cada participante
+
+i=1;
+
+for m=1:(size(Z,1)/1000) ;
+w=W(i:i+999);
+
+M{m}= movmean(w,101);
+
+i=i+1000;
+
+end
+
+%Construindo os graficos
+
+%Dar "clear" sempre que for rodar um gráfico novo
+
+x = linspace(1,1000,1000);
+title('Média Móvel de Acurácia - Completo') %Mudar o nome de acordo com o arquivo aberto
+xlabel("Número da Jogada")
+ylabel("Taxa de acerto")
+
+set(0,'defaultaxescolororder', [[1 0 0]
+                                [0 0 1]
+                                [1 1 0]
+                                [0 0 0]
+                                [1 0 1]
+                                [0 1 1]]);
+                           
+set(0,'defaultaxeslinestyleorder',{'-',':','-.'})
+
+hold on
+for I= 1:(size(Z,1)/1000) ;
+l = num2str(I);
+plot(x,M{I},'LineWidth',2.5,'MarkerSize',2.5,'DisplayName',l)
+ylim([0 1])
+end
+
+xline(334,'--','DisplayName','Intervalo 1');
+xline(668,'--','DisplayName','Intervalo 2');
+
+hold off
+legend show
+
+
+% %%% Contruindo médio móvel temporal
 % for i = 1:size(Z,1);
-%   
-%    if Z(i,8)== Z(i,9);
-%        W(i)= 1;    
-%    else 
-%        W(i)= 0;
+%   P(i)=Z(i,7);
+% %
 %    end
 %    
-%    
-% end
-% 
-% 
 % %Definindo a Cell M composta pelos vetores m equivalentes a média móvel de
 % %cada participante
 % 
 % i=1;
 % 
-% for m=1:(size(Z,1)/1000) ;
-% w=W(i:i+999);
-% 
-% M{m}= movmean(w,101);
+% for p=1:(size(Z,1)/1000) ;
+% u=P(i:i+999);
+% N{p}= movmean(u,101);
 % 
 % i=i+1000;
 % 
@@ -63,7 +114,8 @@ Z=data_full;  %arquivo sendo utilizado
 % %Dar "clear" sempre que for rodar um gráfico novo
 % 
 % x = linspace(1,1000,1000);
-% title('Média Móvel de Acurácia - LTPB') %Mudar o nome de acordo com o arquivo aberto
+% 
+% title('Média Móvel Temporal - Controle') %Mudar o nome de acordo com o arquivo aberto
 % xlabel("Número da Jogada")
 % ylabel("RTs")
 % 
@@ -77,10 +129,10 @@ Z=data_full;  %arquivo sendo utilizado
 % set(0,'defaultaxeslinestyleorder',{'-',':','-.'})
 % 
 % hold on
-% for I= 1:(size(Z,1)/1000) ;
-% l = num2str(I);
-% plot(x,M{I},'LineWidth',2.5,'MarkerSize',2.5,'DisplayName',l)
-% ylim([0 1])
+% for i= 1:(size(Z,1)/1000)
+% l = num2str(i);
+% plot(x,N{i},'LineWidth',2.5,'MarkerSize',2.5,'DisplayName',l)
+% ylim ([0 5])
 % end
 % 
 % xline(334,'--','DisplayName','Intervalo 1');
@@ -88,55 +140,3 @@ Z=data_full;  %arquivo sendo utilizado
 % 
 % hold off
 % legend show
-% 
-
-%%% Contruindo médio móvel temporal
-for i = 1:size(Z,1);
-  P(i)=Z(i,7);
-%
-   end
-   
-%Definindo a Cell M composta pelos vetores m equivalentes a média móvel de
-%cada participante
-
-i=1;
-
-for p=1:(size(Z,1)/1000) ;
-u=P(i:i+999);
-N{p}= movmean(u,101);
-
-i=i+1000;
-
-end
-
-%Construindo os graficos
-
-%Dar "clear" sempre que for rodar um gráfico novo
-
-x = linspace(1,1000,1000);
-
-title('Média Móvel Temporal - Completo') %Mudar o nome de acordo com o arquivo aberto
-xlabel("Número da Jogada")
-ylabel("RTs")
-
-set(0,'defaultaxescolororder', [[1 0 0]
-                                [0 0 1]
-                                [1 1 0]
-                                [0 0 0]
-                                [1 0 1]
-                                [0 1 1]]);
-                           
-set(0,'defaultaxeslinestyleorder',{'-',':','-.'})
-
-hold on
-for i= 1:(size(Z,1)/1000)
-l = num2str(i);
-plot(x,N{i},'LineWidth',2.5,'MarkerSize',2.5,'DisplayName',l)
-ylim ([0 5])
-end
-
-xline(334,'--','DisplayName','Intervalo 1');
-xline(668,'--','DisplayName','Intervalo 2');
-
-hold off
-legend show
