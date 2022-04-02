b28fd7ef3381ef54be191bf09deb85251805f469
vivalent0 <vi.valent.vm@gmail.com>
2022-04-01 21:02:54 -0300

    calculo da performance

--- Rotina_Dados _Quest/analises_e_graficos.m
+++ Rotina_Dados _Quest/analises_e_graficos.m
@@ -27,7 +27,7 @@ data_ltpb = table2array(predata_ltpb);
 
 Z=data_ltpb;  %arquivo sendo utilizado
 [filepath,name,ext] = fileparts(file_control);%Mudar de acordo com o arquivo
-pa = 5; %participante que está sendo analisado.
+pa = 1; %participante que está sendo analisado.
 
 %Presoftware(Z) 
 
@@ -246,76 +246,76 @@ Name=strcat(num2str(pa),space,num2str(Id),space,name) ;
 
 %%% Boxplot de acurácia por bloco 
 
-for i = 1:size(Z,1);
-  
-   if Z(i,8)== Z(i,9);
-       W(i)= 1;    
-   else 
-       W(i)= 0;
-   end
-   
-   
-end
-
-a=1;
-
-for par = 1:(size(Z,1)/1000) ;
-A{par}=W(a:a+999); %Cria um vetor para cada participante dizendo em quais posições cada participante acertou ou errou
-
-a=a+1000;
-
-end
-
-%Bloco 1
-
-AC1 = 0;
-for par = 1:(size(Z,1)/1000);
-    for k = drange(1:334); %numero de jogadas do bloco
-        
-        AC1 = AC1 + A{par}(k); %AC1 -> Acurácia do grupo controle no bloco 1. É a soma dos termos contidos no intervalo k para A{par}
-    end
-    
-    MAC1(par) = AC1/334;  %MAC1 é um vetor em que cada termo corresponde a média de acurácia de um participante no primeiro bloco.
-    AC1=0;
-
-end
-%Bloco 2
-
-AC2 = 0;
-for par = 1:(size(Z,1)/1000);
-    for k = drange(335:668); 
-        
-        AC2 = AC2 + A{par}(k); 
-    end
-    
-    MAC2(par) = AC2/334;  
-    AC2=0;
-end
-
-%Bloco 3
-
-AC3 = 0;
-for par = 1:(size(Z,1)/1000);
-    for k = drange(669:1000); 
-        
-        AC3 = AC3 + A{par}(k); 
-    end
-    
-    MAC3(par) = AC3/332;  
-    AC3=0;
-end
-
-%Boxplot
-
-MACT = [MAC1;MAC2;MAC3].'; %ordena dados em uma matriz para que o comando do boxplot possa funcionar.
-
-BMACT = boxplot(MACT); %boxplot mostrando evolução ao longo dos blocos.
-
-title (strcat('Média de acurácia de cada participante em cada bloco',space,'|',name));
-ylabel("Acurácia");
-xlabel("Bloco");
-ylim([0 1])
-yticks([0:0.2:1])
+% for i = 1:size(Z,1);
+%   
+%    if Z(i,8)== Z(i,9);
+%        W(i)= 1;    
+%    else 
+%        W(i)= 0;
+%    end
+%    
+%    
+% end
+% 
+% a=1;
+% 
+% for par = 1:(size(Z,1)/1000) ;
+% A{par}=W(a:a+999); %Cria um vetor para cada participante dizendo em quais posições cada participante acertou ou errou
+% 
+% a=a+1000;
+% 
+% end
+% 
+% %Bloco 1
+% 
+% AC1 = 0;
+% for par = 1:(size(Z,1)/1000);
+%     for k = drange(1:334); %numero de jogadas do bloco
+%         
+%         AC1 = AC1 + A{par}(k); %AC1 -> Acurácia do grupo controle no bloco 1. É a soma dos termos contidos no intervalo k para A{par}
+%     end
+%     
+%     MAC1(par) = AC1/334;  %MAC1 é um vetor em que cada termo corresponde a média de acurácia de um participante no primeiro bloco.
+%     AC1=0;
+% 
+% end
+% %Bloco 2
+% 
+% AC2 = 0;
+% for par = 1:(size(Z,1)/1000);
+%     for k = drange(335:668); 
+%         
+%         AC2 = AC2 + A{par}(k); 
+%     end
+%     
+%     MAC2(par) = AC2/334;  
+%     AC2=0;
+% end
+% 
+% %Bloco 3
+% 
+% AC3 = 0;
+% for par = 1:(size(Z,1)/1000);
+%     for k = drange(669:1000); 
+%         
+%         AC3 = AC3 + A{par}(k); 
+%     end
+%     
+%     MAC3(par) = AC3/332;  
+%     AC3=0;
+% end
+% 
+% %Boxplot
+% 
+% MACT = [MAC1;MAC2;MAC3].'; %ordena dados em uma matriz para que o comando do boxplot possa funcionar.
+% 
+% BMACT = boxplot(MACT); %boxplot mostrando evolução ao longo dos blocos.
+% 
+% title (strcat('Média de acurácia de cada participante em cada bloco',space,'|',name));
+% ylabel("Acurácia");
+% xlabel("Bloco");
+% ylim([0 1])
+% yticks([0:0.2:1])
 
 %%% Boxplot dos RTs por bloco - Grupo Controle
 % for i = 1:size(Z,1);
@@ -384,4 +384,74 @@ yticks([0:0.2:1])
 % ylim([0 2.5])
 % yticks([0:0.5:3])
 
-%%
+p_mod=[0 1 0
+    0 1 0
+    0 0 1
+    0 0 1
+    0.7 0.3 0];
+P_ctx=[0.23
+    0.10
+    0.10
+    0.23
+    0.33];
+
+%Ocorrencia de cada contexto até derteminada jogada 
+Q=repmat(0,5,1000);
+for mc = drange(1:5)
+    for j = drange(1:1000)
+        if ct_pos(mc,j) ~= 0
+            Q(mc,ct_pos(mc,j))=1;
+            q(mc,j)=sum(Q(mc,1:j));
+        elseif  ct_pos(mc,j) == 0
+    q(mc,j)=sum(Q(mc,1:j));
+        end
+    end
+end    
+%matriz representa as vezes que o participante escolhe a resposta A dado contexto A  
+
+for nc= drange(1:5)
+    Qz=repmat(0,3,1000);
+    qz=repmat(0,3,1000);
+    for jk = drange(1:1000)
+        if Q(nc,jk) == 1 
+            if Z(jk+1,8)==0 
+                Qz(1,jk)= 1;
+            elseif Z(jk+1,8)== 1
+                Qz(2,jk)= 1;
+            elseif Z(jk+1,8)== 2     
+                Qz(3,jk)= 1;
+            end
+        end
+        qz(1,jk)=sum(Qz(1,1:jk));
+        qz(2,jk)=sum(Qz(2,1:jk));
+        qz(3,jk)=sum(Qz(3,1:jk)); 
+    end
+    contz{nc}=qz;
+end
+
+% calculo da probabilidade empirica para os ctx  
+for dc = drange(1:5)   
+    pz=repmat(0,3,1000);
+    for kl=drange(1:1000)
+        pz(1,kl)=contz{dc}(1,kl)/q(dc,kl);
+        pz(2,kl)=contz{dc}(2,kl)/q(dc,kl);
+        pz(3,kl)=contz{dc}(3,kl)/q(dc,kl); 
+    end
+    conty{dc}=pz;
+end  
+ 
+%OBS LIDAR COM O ERRO DE QUANDOA DIVISÃO DÁ 0/0
+
+
+%calculo do indice de performance dos contextos 
+P1=repmat(0,5,1000);
+for lc = drange(1:5)   
+    for kt=drange(1:1000)
+        PI(lc,kt)=1-((abs(conty{lc}(1,kt)-p_mod(lc,1))+abs(conty{lc}(2,kt)-p_mod(lc,2))+abs(conty{lc}(3,kt)- p_mod(lc,3)))/2);
+    end
+end
+for tc = drange(1:5) 
+    for bk = drange(1:1000)
+        PI_final(bk)=sum(PI(1:5,bk))*P_ctx(tc);
+    end
+end    
