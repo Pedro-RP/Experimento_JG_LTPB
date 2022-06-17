b43ac31d458ad3584339835edce3af1017747010
Pedro-RP <ribeirop406@gmail.com>
2022-06-17 15:12:40 -0300

    Update analises_Quest.m

--- Rotina_Dados _Quest/analises_Quest.m
+++ Rotina_Dados _Quest/analises_Quest.m
@@ -7,22 +7,22 @@ Q_ltpb=d_ltpb(1:9,1:12);
 
 %%
 [filepath,name,ext] = fileparts(f_ltpb);%Mudar de acordo com o arquivo
-pa = 8;
+
 %%
 %Comparação idades (Willcoxon test)
 
-% [age_p,age_h]= ranksum(cell2mat(Q_control(2:9,3)),cell2mat(Q_ltpb(2:9,3)))
+% [age_p,age_h]= ranksum(cell2mat(Q_control(2:9,3)),cell2mat(Q_ltpb(3:9,3)))
 
 %%
 %comparação cogtel(Willcoxon test)
 
-[cogtel_p,cogtel_h]= ranksum(cell2mat(Q_control(2:9,8)),cell2mat(Q_ltpb(3:9,8))) %excluindo P002
+%[cogtel_p,cogtel_h]= ranksum(cell2mat(Q_control(2:9,8)),cell2mat(Q_ltpb(3:9,8))) %excluindo P002
 
 
 %%
 %comparação lateralidade(Willcoxon test)
 % 
-% [lat_p,lat_h]= ranksum(cell2mat(Q_control(2:9,9)),cell2mat(Q_ltpb(2:9,9)))
+%[lat_p,lat_h]= ranksum(cell2mat(Q_control(2:9,9)),cell2mat(Q_ltpb(3:9,9)))
 % 
 % 
 % 
@@ -46,7 +46,7 @@ pa = 8;
 % Cfam_freq=[cfam_m,cfam_a,cfam_n];%ocorrencia de respostas muita, alguma e nenhuma
 % Cfam_chi=chi2gof(Cfam_freq)
 % 
-% for i =drange(1:9) ;
+% for i =drange(3:9) ;
 %     if  isequal(Q_ltpb(i,7),{'Muita'  }) 
 %         lfam_m = lfam_m + 1;
 %     elseif  isequal(Q_ltpb(i,7),{'Alguma' })    
@@ -58,58 +58,58 @@ pa = 8;
 % Lfam_freq=[lfam_m,lfam_a,lfam_n];
 % Lfam_chi=chi2gof(Lfam_freq)
 % [tbl,chi2stat,pval] = crosstab(Cfam_freq,Lfam_freq)
-% 
-% 
+
+
 % %%
 % %comparaçao escolaridade(chi-squared)
-% cesc_em = 0;
-% cesc_es = 0;
-% cesc_pos = 0;
-% lesc_em = 0;
-% lesc_es = 0;
-% lesc_pos = 0;
-% for i =drange(1:9) ;
-%     if  isequal(Q_control(i,5),{'Ensino mÃ©dio completo'  }) 
-%         cesc_em = cesc_em + 1;
-%     elseif  isequal(Q_control(i,5),{'Ensino superior completo'})    
-%        cesc_es =cesc_es + 1 ;
-%     elseif isequal(Q_control(i,5),{'PÃ³s-graduaÃ§Ã£o'        })   
-%         cesc_pos = cesc_pos + 1;
-%     end
-% end
-% Cesc_freq=[cesc_em,cesc_es,cesc_pos];%ocorrencia de respostas muita, alguma e nenhuma
-% Cesc_chi=chi2gof(Cesc_freq)
-% for i =drange(1:9) ;
-%     if  isequal(Q_ltpb(i,5),{'Ensino mÃ©dio completo'  }) 
-%         lesc_em = lesc_em + 1;
-%     elseif  isequal(Q_ltpb(i,5),{'Ensino superior completo'})    
-%         lesc_es = lesc_es + 1 ;
-%     elseif isequal(Q_ltpb(i,5),{'PÃ³s-graduaÃ§Ã£o'        })   
-%         lesc_pos = lesc_pos + 1;
-%     end
-% end
-% Lesc_freq=[lesc_em,lesc_es,lesc_pos];
-% Lesc_chi=chi2gof(Lesc_freq)
-% [tbl,chi2stat,pval] = crosstab(Cesc_freq,Lesc_freq)
-% 
+cesc_em = 0;
+cesc_es = 0;
+cesc_pos = 0;
+lesc_em = 0;
+lesc_es = 0;
+lesc_pos = 0;
+for i =drange(1:9) ;
+    if  isequal(Q_control(i,5),{'Ensino mÃ©dio completo'  }) 
+        cesc_em = cesc_em + 1;
+    elseif  isequal(Q_control(i,5),{'Ensino superior completo'})    
+       cesc_es =cesc_es + 1 ;
+    elseif isequal(Q_control(i,5),{'PÃ³s-graduaÃ§Ã£o'        })   
+        cesc_pos = cesc_pos + 1;
+    end
+end
+Cesc_freq=[cesc_em,cesc_es,cesc_pos];%ocorrencia de respostas muita, alguma e nenhuma
+Cesc_chi=chi2gof(Cesc_freq)
+for i =drange(3:9) ;
+    if  isequal(Q_ltpb(i,5),{'Ensino mÃ©dio completo'  }) 
+        lesc_em = lesc_em + 1;
+    elseif  isequal(Q_ltpb(i,5),{'Ensino superior completo'})    
+        lesc_es = lesc_es + 1 ;
+    elseif isequal(Q_ltpb(i,5),{'PÃ³s-graduaÃ§Ã£o'        })   
+        lesc_pos = lesc_pos + 1;
+    end
+end
+Lesc_freq=[lesc_em,lesc_es,lesc_pos];
+Lesc_chi=chi2gof(Lesc_freq)
+[tbl,chi2stat,pval] = crosstab(Cesc_freq,Lesc_freq)
+
 
 %%% Correlação Severidade de Dor x RTs globais médios (Rodar
 %%% analises_e_graficos.m antes)
 
-Severidade = cell2mat(Q_ltpb(3:9,10));
-
-MTTT = Media_temporal_total_L.'; %Transposição da média temporal total_L
-
-RTxS = [MTTT Severidade];
+% Severidade = cell2mat(Q_ltpb(3:9,10));
 % 
-% [R,PValue] = corrplot(RTxS)
+% MTTT = Media_temporal_total_L.'; %Transposição da média temporal total_L
 % 
-% %%% Correlação Interferencia da Dor x RTs globais médios (Rodar
-% %%% analises_e_graficos.m antes)
+% RTxS = [MTTT Severidade];
+% % 
+% % [R,PValue] = corrplot(RTxS)
+% % 
+% % %%% Correlação Interferencia da Dor x RTs globais médios (Rodar
+% % %%% analises_e_graficos.m antes)
+% % 
 % 
-
-Interferencia = cell2mat(Q_ltpb(3:9,11));
-
-RTxI = [MTTT Interferencia];
-
-[R,PValue] = corrplot(RTxI)
+% Interferencia = cell2mat(Q_ltpb(3:9,11));
+% 
+% RTxI = [MTTT Interferencia];
+% 
+% [R,PValue] = corrplot(RTxI)
