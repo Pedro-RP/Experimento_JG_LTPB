1280d2a100b357eee20484a0d809e823f2272889
Pedro-RP <ribeirop406@gmail.com>
2022-09-01 12:18:56 -0300

    Tentando acertar formatação final

--- Rotina_Dados _Quest/Formatacao_JG.py
+++ Rotina_Dados _Quest/Formatacao_JG.py
@@ -4,12 +4,12 @@ from operator import index
 import numpy as np
 import pandas as pd
 
+
 pd.set_option("display.max_rows", None, "display.max_columns", None) #pra os dados serem escritos além do limite de exibição do python#
 
 # create empty lists
 dataframes_list = []
 Dset = pd.DataFrame()
-Dset_list = []
 path=(r"C:\Users\Pedro_R\Desktop\Projeto\Code_exp_ltpb\Rotina_Dados _Quest\dados")
 path2=(r"C:\Users\Pedro_R\Desktop\Projeto\Code_exp_ltpb\Rotina_Dados _Quest\processed")
 #
@@ -21,7 +21,7 @@ for j in range(file_count):
     temp_df = pd.read_csv(path +"/"+ files[j])
     dataframes_list.append(temp_df)
 new_index=list(range(1,1001))
-Dsetwlist=open("dataframe_full.csv",'w')    #cria o arquivo com todos os dados
+#Dsetwlist=open("dataframe_full.csv",'w')    #cria o arquivo com todos os dados
 
  ##reorganizando e escrevendo as tabelas para ficarem compatíveis às rotinas de análise
 count=0
@@ -41,18 +41,19 @@ for dataset in dataframes_list:
             frames = [Dset, data2set.T]
             Dset = pd.concat(frames)
             Dset = Dset.reset_index(drop=True)
-Dset = Dset.set_axis([1,2,3,4,5,6,7,8,9], axis=1, inplace=False)
-Dsetwlist.write(str(Dset))
-Dset_list.append(Dset)
-Dsetwlist.close()
 
- # #criando os arquivos individuais
-df_full = pd.read_csv("dataframe_full.csv" )
+
+Dset = Dset.set_axis([1,2,3,4,5,6,7,8,9], axis=1, inplace=False)
+Dset.to_csv('dataframe_full.csv', index=True)
+ ## #criando os arquivos individuais
+df_full = pd.read_csv("dataframe_full.csv")
+print(df_full)
+
 for i in range (file_count):
-    Df_set=df_full[:1000].to_csv(path2 + "/" +files[i],sep=',',index=False,header=False)
+    Df_set = df_full[:1000].to_csv(path2 + "/" + files[i], sep=',', index=False, header=False)
     df_full = df_full.drop(df_full.index[range(1000)])
     df_full = df_full.drop(df_full.iloc[:, 1:])
-# #print(Df_set)
+#print(Df_set)
 
 #criando os arquivos para os grupos
 files_group = os.listdir(path2)
@@ -101,5 +102,6 @@ for file in files_group:
 
     #print(file[0:4])
 
+print(Dset_Control)
 Dset_Control.close()
 Dset_Patient.close()
