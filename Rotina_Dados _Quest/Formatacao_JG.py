25f675f6b7eb77de33bf003ec5e8a27022f591b5
Pedro-RP <ribeirop406@gmail.com>
2022-09-07 19:38:16 -0300

    colocando apelido do JG no dataframe_full

--- Rotina_Dados _Quest/Formatacao_JG.py
+++ Rotina_Dados _Quest/Formatacao_JG.py
@@ -4,82 +4,94 @@ from operator import index
 import numpy as np
 import pandas as pd
 
+
 pd.set_option("display.max_rows", None, "display.max_columns", None) #pra os dados serem escritos além do limite de exibição do python#
 
 # create empty lists
 dataframes_list = []
 Dset = pd.DataFrame()
-Dset_list = []
-path=(r"C:/Users/vinivius valent/Documents/GitHub/main\Rotina_Dados _Quest\dados")
-path2=(r"C:/Users/vinivius valent/Documents/GitHub/main\Rotina_Dados _Quest\processed")
+path=(r"C:\Users\Pedro_R\Desktop\Projeto\Code_exp_ltpb\Rotina_Dados _Quest\dados")
+path2=(r"C:\Users\Pedro_R\Desktop\Projeto\Code_exp_ltpb\Rotina_Dados _Quest\processed")
 #
-# # #gerando a lista com todos os arquivos
-# files = os.listdir(path)
-# file_count = len(files)
-# print(file_count)
-# for j in range(file_count):
-#     temp_df = pd.read_csv(path +"/"+ files[j])
-#     dataframes_list.append(temp_df)
-# new_index=list(range(1,1001))
-# Dsetwlist=open("dataframe_full.csv",'w')    #cria o arquivo com todos os dados
-#
-# ##reorganizando e escrevendo as tabelas para ficarem compatíveis às rotinas de análise
-# count=0
-# for dataset in dataframes_list:
-#         count+=1
-#         for i in range(dataset.shape[0]):  # Número de linhas
-#             aux = list()
-#             aux = aux + [1] + [1]
-#             aux = aux + [str(dataset.iloc[i, 0])]  # coluna de movimentos
-#             aux = aux + [1]  # Coluna step
-#             aux = aux + [12]  # 12 é o identificador da árvore
-#             aux = aux + [count]  # adicionando identificação alternativa do participante
-#             aux = aux + [str(dataset.iloc[i, 3])]  # adicionando coluna de tempos de respostas
-#             aux = aux + [str(dataset.iloc[i, 2])]  # adicionando coluna de escolhas dos participantes
-#             aux = aux + [str(dataset.iloc[i, 1])]  # adicionando a cadeia do batedor
-#             data2set = pd.DataFrame(aux)
-#             frames = [Dset, data2set.T]
-#             Dset = pd.concat(frames)
-#             Dset = Dset.reset_index(drop=True)
-# Dset = Dset.set_axis([1,2,3,4,5,6,7,8,9], axis=1, inplace=False)
-# Dsetwlist.write(str(Dset))
-# Dset_list.append(Dset)
-# Dsetwlist.close()
-#
-# # #criando os arquivos individuais
-# df_full = pd.read_csv("dataframe_full.csv" )
-# for i in range (file_count):
-#     Df_set=df_full[:1000].to_csv(path2 + "/" +files[i],sep=',',index=False,header=False)
-#     df_full = df_full.drop(df_full.index[range(1000)])
-#     df_full = df_full.drop(df_full.iloc[:, 1:])
-# #print(Df_set)
-#
+ # #Gerando a lista com todos os arquivos
+files = os.listdir(path)
+file_count = len(files)
+#print(file_count)
+for j in range(file_count):
+    temp_df = pd.read_csv(path +"/"+ files[j])
+    dataframes_list.append(temp_df)
+new_index=list(range(1,1001))
+
+ ##reorganizando e escrevendo as tabelas para ficarem compatíveis às rotinas de análise
+count=0
+for dataset in dataframes_list:
+        count+=1
+        for i in range(dataset.shape[0]):  # Número de linhas
+            aux = list()
+            aux = aux + [1] + [1]
+            aux = aux + [str(dataset.iloc[i, 0])]  # coluna de movimentos
+            aux = aux + [1]  # Coluna step
+            aux = aux + [12]  # 12 é o identificador da árvore
+            aux = aux + [count]  # adicionando identificação alternativa do participante
+            aux = aux + [str(dataset.iloc[i, 3])]  # adicionando coluna de tempos de respostas
+            aux = aux + [str(dataset.iloc[i, 2])]  # adicionando coluna de escolhas dos participantes
+            aux = aux + [str(dataset.iloc[i, 1])]  # adicionando a cadeia do batedor
+            data2set = pd.DataFrame(aux)
+            frames = [Dset, data2set.T]
+            Dset = pd.concat(frames)
+
+
+
+ID_full = pd.DataFrame()
+
+for file in files:
+    fl_full=[float(file[1:4])] * 1000
+    IDf= pd.DataFrame(fl_full)
+    ID_full = ID_full.append(IDf)
+    ID_full = ID_full.reset_index(drop=True)
+
+#frames_F = [Dset, ID_full]
+Dset = Dset.reset_index(drop=True)
+Dset = pd.concat([Dset, ID_full], axis= 1)
+Dset = Dset.reset_index(drop=True)
+Dset.to_csv('dataframe_full.csv', index=False, header = False)
+
+ ## #criando os arquivos individuais
+
+df_full = pd.read_csv("dataframe_full.csv",header=None)
+c=0
+
+for i in range (file_count):
+    Df_set = df_full.iloc[c:c+1000,:].to_csv(path2 + "/" + files[i], sep=',', index=False, header=False)
+    c=c+1000
+
 #criando os arquivos para os grupos
+
 files_group = os.listdir(path2)
 file_Controls = len(files_group)
 file_Patients = len(files_group)
 #print(files_group)
 Controls= ["T014","T021","T022","T024","T027","T028","T030","T033"]
-Patients= ["T006","T007","T009","T010","T012","T015","T016","T017"]
+Patients= ["T006","T007","T009","T010","T012","T015","T016","T017"]  #mudar aqui caso seja necessário remover algum participante das análises posteriores.
 dataframes_control = []
 dataframes_patient = []
-Dset_Control=open("dataframe_Control.csv",'w')    #cria o arquivo com todos Controles
-Dset_Patient=open("dataframe_Patient.csv",'w')    #cria o arquivo com todos Pacientes
+
+Dset_Control = pd.DataFrame()
+Dset_Patient = pd.DataFrame()
 
 count1=0
 for file in files_group:
     if file[0:4] in Controls:
         count1 += 1
-        fl=[float(file[1:4])]*1000
-        ID=pd.DataFrame(fl)
+        temp_C = pd.DataFrame(pd.read_csv(path2 + "/" + file, header=None))
+        temp_C= temp_C.set_axis([1,2,3,4,5,6,7,8,9,10], axis=1)
+        temp_C.iloc[:, 5] = count1
+        frames_C = [Dset_Control, temp_C]
+        Dset_Control = pd.concat(frames_C)
+        Dset_Control = Dset_Control.reset_index(drop=True)
 
-        temp_C = pd.DataFrame(pd.read_csv(path2 + "/" + file,delim_whitespace=True,header=None))
-        #temp_C= temp_C.set_axis([1,2,3,4,5,6,7,8,9], axis=1, inplace=False)
-        temp_C.iloc[:,6]=count1
-        temp_C=pd.concat([temp_C ,ID], axis= 1 ,ignore_index=True)
 
 
-        Dset_Control.write(temp_C.to_string(header=False)+"\n")
 
 
 
@@ -88,18 +100,16 @@ count2 = 0
 for file in files_group:
     if file[0:4] in Patients:
         count2 += 1
-        fl =[float(file[1:4])] * 1000
-        ID = pd.DataFrame(fl)
-
-        temp_P = pd.DataFrame(pd.read_csv(path2 + "/" + file ,delim_whitespace=True,header=None))
-        #temp_P = temp_P.set_axis([1, 2, 3, 4, 5, 6, 7, 8, 9], axis=1, inplace=False)
-        temp_P.iloc[:,6] = count2
-        temp_P = pd.concat([temp_P, ID], axis=1, ignore_index=True)
-
-        Dset_Patient.write(temp_P.to_string(header=False)+"\n")
+        temp_P = pd.DataFrame(pd.read_csv(path2 + "/" + file,header=None))
+        temp_P = temp_P.set_axis([1, 2, 3, 4, 5, 6, 7, 8, 9,10], axis=1)
+        temp_P.iloc[:, 5] = count2
+        frames_P = [Dset_Patient, temp_P]
+        Dset_Patient = pd.concat(frames_P)
+        Dset_Patient = Dset_Patient.reset_index(drop=True)
 
 
-    #print(file[0:4])
 
-Dset_Control.close()
-Dset_Patient.close()
+
+
+Dset_Control.to_csv('dataframe_Control.csv', index=False, header = False)
+Dset_Patient.to_csv('dataframe_Patient.csv', index=False, header = False)
--- Rotina_Dados _Quest/analises_e_graficos.m
+++ Rotina_Dados _Quest/analises_e_graficos.m
@@ -1,30 +1,26 @@
 %%Preparando dados para inserção no Goalkeeper's Lab
 
 %%Dados Completos
-% file_full='C:\Users\vinivius valent\Documents\GitHub\main\Rotina_Dados _Quest\dataframe_full.csv';
-% predata_full = readtable(file_full); 
-% predata_full(: , 1) = [ ];
-% data_full = table2array(predata_full);
+file_full='C:\Users\Pedro_R\Desktop\Projeto\Code_exp_ltpb\Rotina_Dados _Quest\dataframe_full.csv';
+predata_full = readtable(file_full);
+data_full = table2array(predata_full);
 
-%%Grupo Controle
-file_control='C:\Users\vinivius valent\Documents\GitHub\main\Rotina_Dados _Quest\dataframe_Control.csv';
-predata_control = readtable(file_control);
-predata_control(: , 1) = [ ]; 
-predata_control(: , 1) = [ ];
-data_control = table2array(predata_control);
-
-%%Grupo LTPB
-
-file_ltpb='C:\Users\vinivius valent\Documents\GitHub\main\Rotina_Dados _Quest\dataframe_Patient.csv';
-predata_ltpb = readtable(file_ltpb);
-predata_ltpb(: , 1) = [ ];
-predata_ltpb(: , 1) = [ ];
-data_ltpb = table2array(predata_ltpb);
-
-Z=data_control;  %arquivo sendo utilizado
-ZC=data_control;
-ZL=data_ltpb;
-% [filepath,name,ext] = fileparts(file_ltpb);%Mudar de acordo com o arquivo
+% %%Grupo Controle
+% file_control='C:\Users\Pedro_R\Desktop\Projeto\Code_exp_ltpb\Rotina_Dados _Quest\dataframe_Control.csv';
+% predata_control = readtable(file_control);
+% data_control = table2array(predata_control);
+% 
+% 
+% %%Grupo LTPB
+% 
+% file_ltpb='C:\Users\Pedro_R\Desktop\Projeto\Code_exp_ltpb\Rotina_Dados _Quest\dataframe_Patient.csv';
+% predata_ltpb = readtable(file_ltpb);
+% data_ltpb = table2array(predata_ltpb);
+% 
+% Z=data_control;  %arquivo sendo utilizado
+% ZC=data_control;
+% ZL=data_ltpb;
+%[filepath,name,ext] = fileparts(file_ltpb);%Mudar de acordo com o arquivo
 % pa = 8; %participante que está sendo analisado.
 
 %%
@@ -254,130 +250,135 @@ ZL=data_ltpb;
 
 %%
 % *% Boxplot de RTs por bloco para os dois grupos*
+% % 
+% % for i = 1:size(ZC,1);
+% %    
+% %     T(i)= ZC(i,7);
+% %    
+% %    
+% % end
+% % 
+% % a=1;
+% % 
+% % for par = 1:(size(ZC,1)/1000) ;
+% % A{par}=T(a:a+999); 
+% % 
+% % a=a+1000;
+% % 
+% % end
+% % 
+% % Bloco 1-controle
+% % 
+% % AC1 = 0;
+% % for par = 1:(size(ZC,1)/1000);
+% %     for k = drange(1:334); %numero de jogadas do bloco
+% %         
+% %         AC1 = AC1 + A{par}(k); %AC1 -> Acurácia do grupo controle no bloco 1. É a soma dos termos contidos no intervalo k para A{par}
+% %     end
+% %     
+% %     MAC1(par) = AC1/334;  %MAC1 é um vetor em que cada termo corresponde a média de acurácia de um participante no primeiro bloco.
+% %     AC1=0;
+% % 
+% % end
+% % Bloco 2-controle
+% % 
+% % AC2 = 0;
+% % for par = 1:(size(ZC,1)/1000);
+% %     for k = drange(335:668); 
+% %         
+% %         AC2 = AC2 + A{par}(k); 
+% %     end
+% %     
+% %     MAC2(par) = AC2/334;  
+% %     AC2=0;
+% % end
+% % 
+% % Bloco 3-controle
+% % 
+% % AC3 = 0;
+% % for par = 1:(size(ZC,1)/1000);
+% %     for k = drange(669:1000); 
+% %         
+% %         AC3 = AC3 + A{par}(k); 
+% %     end
+% %     
+% %     MAC3(par) = AC3/332;  
+% %     AC3=0;
+% % end
+% % 
+% % for i = 1:size(ZL,1);
+% %     
+% %     T2(i)= ZL(i,7);
+% % end     
+% % a=1;
+% % 
+% % for par = 1:(size(ZL,1)/1000) ;
+% % Aa{par}=T2(a:a+999); %Cria um vetor para cada participante contendo seus tempos de resposta
+% % 
+% % 
+% % a=a+1000;
+% % 
+% % end
+% % Bloco 1-ltpb
+% % 
+% % AC4 = 0;
+% % for par = 1:(size(ZL,1)/1000);
+% %     for k = drange(1:334); %numero de jogadas do bloco
+% %         
+% %         AC4 = AC4 + Aa{par}(k); %AC4 -> tempos do grupo controle no bloco 1. É a soma dos termos contidos no intervalo k para A{par}
+% %     end
+% %     
+% %     MAC4(par) = AC4/334;  %MAC1 é um vetor em que cada termo corresponde a média de tempos de um participante no primeiro bloco.
+% %     AC4=0;
+% % 
+% % end
+% % Bloco 2-ltpb
+% % 
+% % AC5 = 0;
+% % for par = 1:(size(ZL,1)/1000);
+% %     for k = drange(335:668); 
+% %         
+% %         AC5 = AC5 + Aa{par}(k); 
+% %     end
+% %     
+% %     MAC5(par) = AC5/334;  
+% %     AC5=0;
+% % end
+% % 
+% % Bloco 3-ltpb
+% % 
+% % AC6 = 0;
+% % for par = 1:(size(ZL,1)/1000);
+% %     for k = drange(669:1000); 
+% %         
+% %         AC6 = AC6 + Aa{par}(k); 
+% %     end
+% %     
+% %     MAC6(par) = AC6/332;  
+% %     AC6=0;
+% % end
+% % 
+% % Boxplot
+% % TMA=[MAC1;MAC4];
+% % TMB=[MAC2;MAC5];
+% % TMC=[MAC3;MAC6];
+% % MACT2 = [TMA;TMB;TMC].'; %ordena dados em uma matriz para que o comando do boxplot possa funcionar.
+% % 
+% % BMACT2 = boxplot(MACT2); %boxplot mostrando evolução ao longo dos blocos.
+% % 
+% % title(strcat('Distribuição de tempo de resposta do grupos em cada bloco'));
+% % ylabel("tempos");
+% % ylim([0 2.5])
+% % yticks([0:0.2:2.5])
+% % xticks([1.5 3.5 5.5])
+% % xticklabels({'1° Bloco','2° Bloco','3° Bloco'})
+% % xline(2.5)
+% % xline(4.5)
+% % 
+% % %Verificando quais participantes apresentam médias temporais nos últimos blocos
+%%maiorees do que as do primeiro
 
-for i = 1:size(ZC,1);
-   
-    T(i)= ZC(i,7);
-   
-   
-end
 
-a=1;
-
-for par = 1:(size(ZC,1)/1000) ;
-A{par}=T(a:a+999); 
-
-a=a+1000;
-
-end
-
-%Bloco 1-controle
-
-AC1 = 0;
-for par = 1:(size(ZC,1)/1000);
-    for k = drange(1:334); %numero de jogadas do bloco
-        
-        AC1 = AC1 + A{par}(k); %AC1 -> Acurácia do grupo controle no bloco 1. É a soma dos termos contidos no intervalo k para A{par}
-    end
-    
-    MAC1(par) = AC1/334;  %MAC1 é um vetor em que cada termo corresponde a média de acurácia de um participante no primeiro bloco.
-    AC1=0;
-
-end
-%Bloco 2-controle
-
-AC2 = 0;
-for par = 1:(size(ZC,1)/1000);
-    for k = drange(335:668); 
-        
-        AC2 = AC2 + A{par}(k); 
-    end
-    
-    MAC2(par) = AC2/334;  
-    AC2=0;
-end
-
-%Bloco 3-controle
-
-AC3 = 0;
-for par = 1:(size(ZC,1)/1000);
-    for k = drange(669:1000); 
-        
-        AC3 = AC3 + A{par}(k); 
-    end
-    
-    MAC3(par) = AC3/332;  
-    AC3=0;
-end
-
-for i = 1:size(ZL,1);
-    
-    T2(i)= ZL(i,7);
-end     
-a=1;
-
-for par = 1:(size(ZL,1)/1000) ;
-A{par}=T2(a:a+999); %Cria um vetor para cada participante contendo seus tempos de resposta
-
-
-a=a+1000;
-
-end
-%Bloco 1-ltpb
-
-AC4 = 0;
-for par = 1:(size(ZL,1)/1000);
-    for k = drange(1:334); %numero de jogadas do bloco
-        
-        AC4 = AC4 + A{par}(k); %AC4 -> tempos do grupo controle no bloco 1. É a soma dos termos contidos no intervalo k para A{par}
-    end
-    
-    MAC4(par) = AC4/334;  %MAC1 é um vetor em que cada termo corresponde a média de tempos de um participante no primeiro bloco.
-    AC4=0;
-
-end
-%Bloco 2-ltpb
-
-AC5 = 0;
-for par = 1:(size(ZL,1)/1000);
-    for k = drange(335:668); 
-        
-        AC5 = AC5 + A{par}(k); 
-    end
-    
-    MAC5(par) = AC5/334;  
-    AC5=0;
-end
-
-%Bloco 3-ltpb
-
-AC6 = 0;
-for par = 1:(size(ZL,1)/1000);
-    for k = drange(669:1000); 
-        
-        AC6 = AC6 + A{par}(k); 
-    end
-    
-    MAC6(par) = AC6/332;  
-    AC6=0;
-end
-
-%Boxplot
-TMA=[MAC1;MAC4];
-TMB=[MAC2;MAC5];
-TMC=[MAC3;MAC6];
-MACT2 = [TMA;TMB;TMC].'; %ordena dados em uma matriz para que o comando do boxplot possa funcionar.
-
-BMACT2 = boxplot(MACT2); %boxplot mostrando evolução ao longo dos blocos.
-
-title(strcat('Distribuição de tempo de resposta do grupos em cada bloco'));
-ylabel("tempos");
-ylim([0 2.5])
-yticks([0:0.2:2.5])
-xticks([1.5 3.5 5.5])
-xticklabels({'1° Bloco','2° Bloco','3° Bloco'})
-xline(2.5)
-xline(4.5)
 
 %%
 % %%COntruindo calculo da performance individual
