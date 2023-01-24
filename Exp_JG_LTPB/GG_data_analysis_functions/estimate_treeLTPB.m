4f24c2b55685681f030c5f5f3b4371849cd91837
Pedro-RP <ribeirop406@gmail.com>
2023-01-24 19:01:08 -0300

    adding LTPB trees

--- Exp_JG_LTPB/GG_data_analysis_functions/estimate_treeLTPB.m
+++ Exp_JG_LTPB/GG_data_analysis_functions/estimate_treeLTPB.m
@@ -3,7 +3,6 @@
 alphal = 3;
 
 
-
 % Control group
 
 for i = 1:size(data_control,1)
@@ -38,14 +37,12 @@ b=b+1000;
 end
 
 nrowsC = ceil(((size(data_control,1)/1000)/3));
-ncolC = fix(((size(data_control,1)/1000)/3));
-
 
 figure
 
 for par = 1:(size(data_control,1)/1000) 
 
-subplot(nrowsC,ncolC, par)
+subplot(nrowsC,3, par)
 [tau_est1] = tauest_RT(alphal, RT1{par}, P1{par}, 1);
 
 title(append('C ', num2str(par)));
@@ -56,41 +53,55 @@ end
 
 suptitle('Context Trees of the Control Group')
 
-% % LTPB group
-% 
-% 
-% for i = 1:size(data_LTPB,1)
-% 
-%     T2(i)= data_LTPB(i,7);
-% 
-% 
-% end
-% 
-% a=1;
-% 
-% for par = 1:(size(data_LTPB,1)/1000) 
-% RT2{par}=T2(a:a+999); 
-% 
-% a=a+1000;
-% 
-% end
-% 
-% for pos = 1:size(data_LTPB,1)
-% 
-%     p2(pos)= data_LTPB(pos,9);
-% 
-% end
-% 
-% b=1;
-% 
-% for par = 1:(size(data_LTPB,1)/1000) 
-%     
-% P2{par}=p2(b:b+999); 
-% 
-% b=b+1000;
-% 
-% end
-% 
-% figure
-% [tau_est2] = tauest_RT(alphal, RT2{10}, P2{10}, 1);
+% LTPB group
+
+
+for i = 1:size(data_LTPB,1)
+
+    T2(i)= data_LTPB(i,7);
+
+
+end
+
+a=1;
+
+for par = 1:(size(data_LTPB,1)/1000) 
+RT2{par}=T2(a:a+999); 
+
+a=a+1000;
+
+end
+
+for pos = 1:size(data_LTPB,1)
+
+    p2(pos)= data_LTPB(pos,9);
+
+end
+
+b=1;
+
+for par = 1:(size(data_LTPB,1)/1000) 
+    
+P2{par}=p2(b:b+999); 
+
+b=b+1000;
+
+end
+
+nrowsL = ceil(((size(data_LTPB,1)/1000)/3));
+
+figure
+
+for par = 1:(size(data_LTPB,1)/1000) 
+
+subplot(nrowsL,3, par)
+[tau_est2] = tauest_RT(alphal, RT2{par}, P2{par}, 1);
+
+title(append('L ', num2str(par)));
+
+tau_estL{par} = tau_est2;
+
+end
+
+suptitle('Context Trees of the LTPB Group')
 
